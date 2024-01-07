662268061e071b466d77aca4153e4ce62d824f6b
Gerson Sánchez <gb.so.dev@outlook.com>
2024-01-07 14:33:59 -0500

    Entity: Development unit test

---
+++ Clean.Domain.Entity.Test/Builder/UserBuilder.cs
@@ -0,0 +1,41 @@
+﻿using Clean.Domain.Entity.Test.TestModel;
+
+namespace Clean.Domain.Tests.Builders
+{
+	public class UserBuilder
+	{
+		private Guid _id = Guid.NewGuid();
+		private string _name = "Default Name";
+		private string _email = "default@example.com";
+
+		public UserBuilder WithId(Guid id)
+		{
+			_id = id;
+			return this;
+		}
+
+		public UserBuilder WithName(string name)
+		{
+			_name = name;
+			return this;
+		}
+
+		public UserBuilder WithEmail(string email)
+		{
+			_email = email;
+			return this;
+		}
+
+		public User Build(bool isToUpdate)
+		{
+			if (isToUpdate)
+			{
+				return new User(_id, _name, _email);
+			}
+			else
+			{
+				return new User(_name, _email);
+			}
+		}
+	}
+}
