3b03aab47ee77b80b9e76c6a7efbf9bf237626e4
Gerson Sánchez <gb.so.dev@outlook.com>
2024-01-07 14:48:58 -0500

    Exceptions: develop

---
+++ Clean.Domain.Exceptions/ApplicationExeption.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Clean.Domain.Exceptions
+{
+	public class ApplicationExeption : Exception
+	{
+
+		public ApplicationExeption(string mensaje)
+			: base(mensaje)
+		{
+		}
+
+		public ApplicationExeption(string message, Exception innerException)
+			: base(message, innerException)
+		{
+		}
+	}
+}
