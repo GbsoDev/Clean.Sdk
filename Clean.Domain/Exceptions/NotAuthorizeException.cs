16531e8aa586db4daecc47e23fe478f5151e2ee3
GbsoDev <gb.so.dev@outlook.com>
2024-01-13 20:31:56 -0500

    Domain: Development domain

---
+++ Clean.Domain/Exceptions/NotAuthorizeException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Clean.Domain.Exceptions
+{
+	public class NotAuthorizeException
+		: ValidationException
+	{
+		public NotAuthorizeException(string? message) : base(message)
+		{
+		}
+
+		public NotAuthorizeException(string? message, Exception? innerException) : base(message, innerException)
+		{
+		}
+	}
+}
