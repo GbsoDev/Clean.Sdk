044e51bf20c7a3df42958d127034513fb52bf049
GbsoDev <gb.so.dev@outlook.com>
2024-01-13 22:45:30 -0500

    Services: Development services

---
+++ Clean.Domain.Services/IDeleteService.cs
@@ -0,0 +1,13 @@
+﻿using Clean.Domain.Entity;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Clean.Domain.Services
+{
+	public interface IDeleteService<TEntity>
+		where TEntity : class, IDomainEntity
+	{
+		Task DeleteAsync(TEntity entity, CancellationToken cancellationToken);
+		Task DeleteByIdAsync(object id, CancellationToken cancellationToken);
+	}
+}
