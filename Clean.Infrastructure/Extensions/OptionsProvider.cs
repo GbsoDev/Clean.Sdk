7ffb7d745afe7af1a6b2e5a68e9fcf70c5bf4a21
GbsoDev <gb.so.dev@outlook.com>
2024-04-28 23:38:44 -0500

    Domain: Infrastructure, se actualiza proveedor de configuraciones

--- Clean.Infrastructure/Extensions/OptionsProvider.cs
+++ Clean.Infrastructure/Extensions/OptionsProvider.cs
@@ -2,34 +2,78 @@ using Clean.Domain.Helpers;
 using Clean.Domain.Options;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using System.Reflection;
 
 namespace Clean.Infrastructure.Extensions
 {
 	public static class OptionsProvider
 	{
+		/// <summary>
+		/// Ruta de archivos de configuración: appsettings.json
+		/// </summary>
+		public static string? ConfigurationFilePath { get; set; }
 		private const string DEVELOPMENT_SUFIX = "Development";
-		public static string ConfigurationFilePath { get; set; } = "appsettings.json";
-		public static string ConfigurationDevelopmentFilePath { get => $"{Path.GetFileNameWithoutExtension(ConfigurationFilePath)}.{DEVELOPMENT_SUFIX}.{Path.GetExtension(ConfigurationFilePath)}"; }
+		private static string ConfigurationDevelopmentFilePath => $"{Path.GetFileNameWithoutExtension(ConfigurationFilePath)}.{DEVELOPMENT_SUFIX}{Path.GetExtension(ConfigurationFilePath)}";
+		private static Action<BinderOptions> BinderOptions => options => options.BindNonPublicProperties = true;
 
-		public static IServiceCollection AddAppSettings<T>(this IServiceCollection services, ref IConfiguration configuration, out T appSettings)
+		public static IServiceCollection ConfigureAppSettingOptions<T>(this IServiceCollection services, ref IConfiguration configuration, out T appSettings, bool singleton = true)
 			where T : AppSettings, new()
 		{
 			appSettings = new T();
-			var builder = new ConfigurationBuilder();
-			configuration = builder.AddConfiguration(configuration)
-				.AddJsonFile(ConfigurationFilePath, true, true)
+			IConfigurationBuilder configurationBuilder = new ConfigurationBuilder()
+				.AddConfiguration(configuration);
+			if (ConfigurationFilePath is not null)
+			{
+				configurationBuilder = configurationBuilder.AddJsonFile(ConfigurationFilePath, true, true);
 #if (DEBUG)
-				.AddJsonFile(ConfigurationDevelopmentFilePath, true, true)
+				configurationBuilder = configurationBuilder.AddJsonFile(ConfigurationDevelopmentFilePath, true, true);
 #endif
-				.AddEnvironmentVariables()
-				.Build();
-			configuration.Bind(appSettings);
-			services.AddSingleton(appSettings);
+			}
+			configurationBuilder = configurationBuilder.AddEnvironmentVariables();
+
+			configuration = configurationBuilder.Build();
+			configuration.Bind(appSettings, BinderOptions);
+
+			services.AddSingleton(configuration);
+			services.AddSingleton((IConfigurationRoot)configuration);
+			if (singleton)
+			{
+				services.ConfigureSingletonOptions(configuration, AssemblyHelper.GetCleanDomainAssembly);
+				services.Configure<T>(configuration, BinderOptions);
+			}
+			else
+			{
+				services.ConfigureScopedOptions(AssemblyHelper.GetCleanDomainAssembly);
+				services.AddScoped<IOptions<T>>(serviceProvider => serviceProvider.BindOptions<T>());
+			}
 			return services;
 		}
 
-		public static IServiceCollection AddOptions(this IServiceCollection services, IConfiguration configuration, Assembly assembly)
+		public static IServiceCollection ConfigureScopedOptions(this IServiceCollection services, Assembly assembly)
+		{
+			return services.ConfigureOptions(null, assembly);
+		}
+
+		public static IServiceCollection ConfigureSingletonOptions(this IServiceCollection services, IConfiguration configuration, Assembly assembly)
+		{
+			return services.ConfigureOptions(configuration, assembly);
+		}
+
+		public static void ConfigureSingleton<TOptions>(IServiceCollection services, IConfiguration configuration, string? secctionName = null)
+			where TOptions : class, new()
+		{
+			var configurationSection = string.IsNullOrEmpty(secctionName) ? configuration : configuration.GetSection(secctionName);
+			services.Configure<TOptions>(option => configurationSection.Bind(option, BinderOptions));
+		}
+
+		public static void ConfigureScoped<TOptions>(IServiceCollection services, string secctionName)
+			where TOptions : class, new()
+		{
+			services.AddScoped<IOptions<TOptions>>(serviceProvider => serviceProvider.BindOptions<TOptions>(secctionName));
+		}
+
+		private static IServiceCollection ConfigureOptions(this IServiceCollection services, IConfiguration? configuration, Assembly assembly)
 		{
 			var types = AssemblyHelper.GeyTypesByAttribute(assembly, typeof(OptionAttribute));
 			foreach (var type in types)
@@ -38,16 +82,85 @@ namespace Clean.Infrastructure.Extensions
 				var constructor = type.GetConstructor(new Type[] { });
 				if (constructor != null)
 				{
-					var instance = constructor.Invoke(null);
-					AddOption(instance, services, configuration, attribute.SecctionName);
+					if (configuration != null)
+					{
+						services.ConfigureSingleton(type, configuration, attribute.SecctionName);
+					}
+					else
+					{
+						services.ConfigureScoped(type, attribute.SecctionName);
+					}
 				}
 			}
 			return services;
 		}
 
-		private static void AddOption<T>(T Instance, IServiceCollection services, IConfiguration configuration, string secctionName) where T : class
+		private static void ConfigureSingleton(this IServiceCollection services, Type type, IConfiguration configuration, string? sectionName = null)
 		{
-			services.Configure<T>(instance => configuration.GetSection(secctionName).Bind(instance));
+			const string methodName = nameof(ConfigureSingleton);
+			var classType = typeof(OptionsProvider);
+
+			MethodInfo? configureMethod = classType
+				.GetMethod(
+					methodName,
+					BindingFlags.Static | BindingFlags.NonPublic,
+					null,
+					new[] { typeof(IServiceCollection), typeof(IConfiguration), typeof(string) },
+					null
+				);
+
+			if (configureMethod != null)
+			{
+				configureMethod.MakeGenericMethod(type)
+					.Invoke(null, new object[] { services, configuration, sectionName! });
+			}
+			else
+			{
+				throw new Clean.Domain.Exceptions.AppExeption($"Error: Method {classType.FullName}.{methodName} not found");
+			}
+		}
+
+		private static void ConfigureScoped(this IServiceCollection services, Type type, string? sectionName = null)
+		{
+			const string methodName = nameof(ConfigureScoped);
+			var classType = typeof(OptionsProvider);
+
+			MethodInfo? configureMethod = classType
+				.GetMethod(
+					methodName,
+					BindingFlags.Static | BindingFlags.Public,
+					null,
+					new[] { typeof(IServiceCollection), typeof(string) },
+					null
+				);
+
+			if (configureMethod != null)
+			{
+				configureMethod.MakeGenericMethod(type)
+					.Invoke(null, new object[] { services, sectionName! });
+			}
+			else
+			{
+				throw new Clean.Domain.Exceptions.AppExeption($"Error: Method {classType.FullName}.{methodName} not found");
+			}
+		}
+
+		private static CustomOption<TOptions> BindOptions<TOptions>(this IServiceProvider serviceProvider, string? sectionName = null) where TOptions : class, new()
+		{
+			var config = serviceProvider.GetRequiredService<IConfiguration>();
+			config = string.IsNullOrEmpty(sectionName) ? config : config.GetSection(sectionName);
+
+			return new(config.Get<TOptions>(BinderOptions)!);
+		}
+	}
+
+	public class CustomOption<TOptions> : IOptions<TOptions> where TOptions : class
+	{
+		public TOptions Value { private set; get; }
+
+		public CustomOption(TOptions value)
+		{
+			Value = value;
 		}
 	}
 }
